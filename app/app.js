6994cd46c1239b1db6a321b9125cadbdace7be7e
Daniel Weisdorf <dw1236@gmail.com>
2020-06-12 09:17:53 -0400

    medium editor

--- app/app.js
+++ app/app.js
@@ -4,20 +4,48 @@ let app = angular.module("myApp", []);
 
 // This isn't a big enough application to warrant multi-page and splitting out of functions into separate controllers etc.
 app.controller("MainController", function ($scope) {
-  $scope.page = 0;
-  if (localStorage.getItem("token")) {
-    $scope.page = 1;
-  }
+  // Initial run
   let provider = new firebase.auth.GoogleAuthProvider();
+  let database = firebase.database();
 
+  $scope.page = 0;
+
+  let mediumEditor = new MediumEditor(".editable");
+
+  mediumEditor.subscribe("editableInput", function () {
+    database.ref("users/" + localStorage.getItem("token")).set({
+      data: mediumEditor.getContent(),
+    });
+  });
+
+  let queueEditor = async function () {
+    $scope.page = 1;
+
+    database
+      .ref("users/" + localStorage.getItem("token"))
+      .once("value")
+      .then(function (dataSnapshot) {
+        mediumEditor.setContent(dataSnapshot.val().data);
+      });
+  };
+
+  if (localStorage.getItem("token")) {
+    queueEditor();
+  }
+
+  // Functions for later
   $scope.signIn = function () {
     firebase
       .auth()
       .signInWithPopup(provider)
       .then(function (result) {
-        localStorage.setItem("token", result.credential.accessToken); // this is the epitome of safety
+        let email = result.user.email;
+        let userId = email.substring(0, email.indexOf("@")).replace(".", "");
+
+        localStorage.setItem("token", userId); // easy unique key for the json
+        console.log(result.user.email);
         $scope.$apply(function () {
-          $scope.page = 1;
+          queueEditor();
         });
       })
       .catch(function () {
