8b76efe56ffb4fd1b6813e7fcc32538cabeab81e
Daniel Weisdorf <dw1236@gmail.com>
2020-06-12 07:47:55 -0400

    stripped out routing, added firebase, ser up authentication and basic flipping between pages, very ugly state right now

--- app/app.js
+++ app/app.js
@@ -1,12 +1,32 @@
 "use strict";
 
-// Declare app level module which depends on views, and core components
-angular.module("myApp", ["ngRoute", "myApp.view1", "myApp.view2"]).config([
-  "$locationProvider",
-  "$routeProvider",
-  function ($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix("!");
+let app = angular.module("myApp", []);
 
-    $routeProvider.otherwise({ redirectTo: "/view1" });
-  },
-]);
+// This isn't a big enough application to warrant multi-page and splitting out of functions into separate controllers etc.
+app.controller("MainController", function ($scope) {
+  $scope.page = 0;
+  if (localStorage.getItem("token")) {
+    $scope.page = 1;
+  }
+  let provider = new firebase.auth.GoogleAuthProvider();
+
+  $scope.signIn = function () {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        localStorage.setItem("token", result.credential.accessToken); // this is the epitome of safety
+        $scope.$apply(function () {
+          $scope.page = 1;
+        });
+      })
+      .catch(function () {
+        alert("Login seems to have failed, maybe try again?");
+      });
+  };
+
+  $scope.logout = function () {
+    localStorage.removeItem("token");
+    $scope.page = 0;
+  };
+});
